e298a1f46aca0270ec4f3bc4320b5f74caa5bbcf
Ritik Raj <ritik@air.local>
2024-12-08 16:54:28 +0530

    initialize project and bind to port

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import socket  # noqa: F401
+
+
+def create_message(id):
+    id_bytes = id.to_bytes(4, byteorder="big")
+    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+
+
+def handle_client(client):
+    client.recv(1024)
+    client.sendall(create_message(7))
+    client.close()
+
+
+def main():
+    # You can use print statements as follows for debugging,
+    # they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    # Uncomment this to pass the first stage
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+    # server.accept() # wait for client
+    while True:
+        client, _ = server.accept()
+        handle_client(client)
+
+
+if __name__ == "__main__":
+    main()
