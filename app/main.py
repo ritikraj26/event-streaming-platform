5dc7abcc7d0c0fb8a8172348e4727cdfa2622527
Ritik Raj <ritik@air.local>
2024-12-12 01:18:57 +0530

    Parse api version and handle api version requests

--- app/main.py
+++ app/main.py
@@ -1,28 +1,64 @@
-import socket  # noqa: F401
+import socket
+from dataclasses import dataclass
+from enum import Enum, unique
 
+@unique
+class ErrorCode(Enum):
+    NONE = 0
+    UNSUPPORTED_VERSION = 35
 
-def create_message(id):
-    id_bytes = id.to_bytes(4, byteorder="big")
-    response = len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+@dataclass
+class KafkaRequest:
+    api_key: int
+    api_version: int
+    correlation_id: int
+
+    @staticmethod
+    def from_client(client: socket.socket):
+        data = client.recv(2048)
+        return KafkaRequest(
+            api_key = int.from_bytes(data[4:6], byteorder="big"),
+            api_version = int.from_bytes(data[6:8], byteorder="big"),
+            correlation_id = int.from_bytes(data[8:12], byteorder="big")
+        )
+
+def create_message(request: KafkaRequest):
+    response_header = request.correlation_id.to_bytes(4, byteorder="big")
+    valid_api_version = [0, 1, 2, 3, 4]
+    error_code = (
+        ErrorCode.NONE
+        if request.api_version in valid_api_version
+        else ErrorCode.UNSUPPORTED_VERSION
+    )
+    response_header += error_code.value.to_bytes(2, byteorder="big")
+
+    min_version, max_version = 0, 4
+    throttle_time_ms = 0
+    tag_buffer = b"\x00"
+    response_body = (
+        int(2).to_bytes(1)
+        + request.api_key.to_bytes(2)
+        + min_version.to_bytes(2)
+        + max_version.to_bytes(2)
+        + tag_buffer
+        + throttle_time_ms.to_bytes(4)
+        + tag_buffer
+    )
+
+    response_length = len(response_header) + len(response_body)
+    response = response_length.to_bytes(4, byteorder="big") + response_header + response_body
     return response
 
 
 def handle_client(client):
-    req = client.recv(1024)
-    corelation_id = int.from_bytes(req[8:12], byteorder="big")
-    print(corelation_id)
-    client.sendall(create_message(corelation_id))
+    request = KafkaRequest.from_client(client)
+    client.sendall(create_message(request))
     client.close()
 
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    # server.accept() # wait for client
+    # server.accept()
     while True:
         client, _ = server.accept()
         handle_client(client)
